4524d5bf3fbfc3966b12e6fa32e644b601b1b186
vosse <jovicevicdev@gmail.com>
2025-01-08 23:52:17 +0100

    feat: add saint patrons day page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import  { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Landing } from './Components/Landing'
 import { About } from './Components/About'
 import { Contact } from './Components/Contact'
+import { Slava } from './Components/Slava'
 import BackButton from './Components/BackButton'
 
 export const App = () => {
@@ -17,6 +18,7 @@ export const App = () => {
                 <Route exact path='/' component={ Landing } />
                 <Route exact path='/about' component={ About } />
                 <Route exact path='/contact' component={ Contact } />
+                <Route exact path='/srecna_slava' component= { Slava } />
                 <Route render={() => <Landing /> } />
             </Switch>
         </BrowserRouter>
