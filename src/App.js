a4e3db996edc0bb2764ccd8fc996e22c2763612a
vosse <jovicevicdev@gmail.com>
2025-05-28 23:29:44 +0200

    feat: update resume

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import  { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Landing } from './Components/Landing'
 import { About } from './Components/About'
 import { Contact } from './Components/Contact'
-import { Slava } from './Components/Slava'
 import BackButton from './Components/BackButton'
 
 export const App = () => {
@@ -18,7 +17,6 @@ export const App = () => {
                 <Route exact path='/' component={ Landing } />
                 <Route exact path='/about' component={ About } />
                 <Route exact path='/contact' component={ Contact } />
-                <Route exact path='/srecna_slava' component= { Slava } />
                 <Route render={() => <Landing /> } />
             </Switch>
         </BrowserRouter>
